ddab43f9ccfc5adc50b247d98e0862c7f2ef8fdd
Mailis <mailis.kokla@gmail.com>
2018-01-15 14:12:14 +0200

    Fixed loading positioning

--- src/components/Battle.vue
+++ src/components/Battle.vue
@@ -96,11 +96,6 @@ export default {
   width: 100%;
   height: 100%;
   background-color: rgba(0, 0, 0, 0.8);
-  position: absolute;
-  top: 0;
-  right: 0;
-  left: 0;
-  bottom: 0;
 }
 .vue-loading-container {
   position: absolute;
